f3d99872a002f9c3dd7b6ec550ea7cc25e99e4c3
Jessica Bertschi <161706812+jessizhx@users.noreply.github.com>
2024-06-20 09:45:24 +0200

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <img alt="BBW-Winterthur" src="/images/logo.png" />
+          </li>
+        </ul>
+      </nav>
+      <h4>Welcome to Data- Grids - Tables in React</h4>
+    </header>
+  );
+}
